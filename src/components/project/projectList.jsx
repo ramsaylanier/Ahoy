8b09b74375e900d79dbefe052dd696402ff1bebe
Ramsay Lanier <ramsaylanier@gmail.com>
2019-04-06 05:40:02 -0400

    fix projectList

--- src/components/project/projectList.jsx
+++ src/components/project/projectList.jsx
@@ -1,17 +1,30 @@
 /** @jsx jsx */
 import { useState } from "react"
+import { jsx, css } from "@emotion/core"
 import PropTypes from "prop-types"
 import posed from "react-pose"
+import theme, { lightenPrimary } from "@/theme"
 
 import ProjectListItem from "./projectListItem"
 
 import ExpandRightIcon from "@/icons/expandRightIcon"
 import ExpandLeftIcon from "@/icons/expandLeftIcon"
 
-import theme, { lightenPrimary } from "@/theme"
-import { jsx, css } from "@emotion/core"
+const List = posed.div({
+  open: { width: 300 },
+  close: { width: 60 }
+})
+
+const list = css`
+  display: flex;
+  flex-flow: column;
+  background: white;
+`
 
 const listHeader = css`
+  display: flex;
+  justify-content: flex-start;
+  padding: 0.5rem 0;
   background: ${lightenPrimary(1.3)};
 `
 
